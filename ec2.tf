306ce8e154d348e2fedc847230ef915181c1a741
venn1991 <vennr456@gmail.com>
2025-03-25 10:45:08 +0530

    1st commit

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "web" {
+    ami  = "ami-084568db4383264d4"
+    instance_type = "t2.micro"
+    key_name = "today-react"
+    tags = {
+        Name = "vmr-vcube133134"
+    }
+  
+}
